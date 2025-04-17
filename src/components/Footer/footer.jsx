316a2f5b9784392699aebb0add50c39b3ae1c414
Ashley Pow <ashleypow@Ashleys-MacBook-Pro.local>
2025-04-17 12:19:36 +0530

    Changed footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bg-gray-900 text-white py-4">
             <div className="container mx-auto flex justify-center">
                 <p className="text-sm">
-                    &copy; {new Date().getFullYear()} TCET COMP A Roll No. 61-65. All rights reserved.
+                    &copy; {new Date().getFullYear()} TCET COMP A Roll No. 12, 65, 66, 68. All rights reserved.
                 </p>
             </div>
         </footer>
